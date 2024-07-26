f2f9f2de109be82f3f91ace0e53810270ea655c3
azure2_jamie <jamie_hsu@manka.co.jp>
2024-07-26 17:07:42 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -5,22 +5,13 @@
 	const imageUrl = ref<string>("");
 	const totalImages = ref<number | null>(null);
 
-	const fetchData = async (url: string): Promise<any> => {
-		try {
-			const response = await fetch(url);
-			if (!response.ok) throw new Error("Network response was not ok");
-			return response.json();
-		} catch (error) {
-			console.error("Fetch error:", error);
-			throw error;
-		}
-	};
-
 	const loadImage = async () => {
+		const getRandomtanuki = `https://random-tanuki.vercel.app/api/randomtanuki/?i=${index.value}`;
+
 		try {
-			const data = await fetchData(
-				`https://random-tanuki.vercel.app/api/randomTanuki/?i=${index.value}`,
-			);
+			const response = await fetch(getRandomtanuki);
+			const data = await response.json();
+			console.log("API 返回的數據:", data);
 			if (data.url) {
 				imageUrl.value = data.url;
 				totalImages.value = data.totalImages;
@@ -29,41 +20,44 @@
 				alert("圖片加載錯誤");
 			}
 		} catch (error) {
+			console.error("圖片加載錯誤:", error);
 			imageUrl.value = "";
 			alert("圖片加載錯誤");
 		}
 	};
-
+	// 在組件加載時檢查網址中的查詢參數
 	onMounted(async () => {
-		try {
-			const data = await fetchData("https://random-tanuki.vercel.app/api/totalTanukis");
-			randomTanukiStore.totalImages = data.totalImages;
+		const getTotalTanukis = await fetch(`https://random-tanuki.vercel.app/api/totalTanukis`);
 
-			const query = new URLSearchParams(window.location.search);
-			const i = query.get("i");
+		const data = await getTotalTanukis.json();
 
-			if (i) {
-				index.value = Number(i);
-			} else {
-				index.value = Math.floor(Math.random() * randomTanukiStore.totalImages!);
-			}
+		randomTanukiStore.totalImages = data.totalImages;
 
-			await loadImage();
-		} catch (error) {
-			console.error("初始數據加載錯誤:", error);
+		const query = new URLSearchParams(window.location.search);
+		const i = query.get("i");
+
+		if (i) {
+			index.value = Number(i);
+		} else {
+			// 如果沒有查詢參數，生成一個隨機索引
+			index.value = Math.floor(Math.random() * randomTanukiStore.totalImages!); // 確保 totalImages 有值
 		}
+
+		await loadImage();
 	});
 </script>
 
 <template>
 	<div class="flex-v-c w-full h-full">
-		<div class="flex-h-c w-full">
+		<div
+			class="flex-h-c w-full"
+			v-if="imageUrl">
 			<div class="w-[1000px] aspect-[5/3] flex-v-c overflow-hidden">
 				<img
 					class="w-full h-full object-cover"
 					:src="imageUrl"
-					alt="二!狸貓" />
-			</div>
+					alt="隨機圖片"
+			/></div>
 		</div>
 	</div>
 </template>
