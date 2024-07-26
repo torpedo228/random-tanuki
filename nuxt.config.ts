623c8e68bdce466a496388750ad9c452accc4b19
azure2_jamie <jamie_hsu@manka.co.jp>
2024-07-26 17:05:10 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  // runtimeConfig: {
-  //   public: {
-  //     // apiBaseUrl: "/api",
-  //     apiBaseUrl: "https://random-tanuki.vercel.app/api",
-  //   },
-  // },
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
