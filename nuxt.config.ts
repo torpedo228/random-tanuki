72c24138b0832edcbf90328fdabea1bb34c05e47
azure2_jamie <jamie_hsu@manka.co.jp>
2024-07-26 16:03:54 +0900

    test vercel deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL_DEV,
-      apiImgUrl: process.env.API_IMG_URL_DEV,
+      // apiBaseUrl: process.env.API_BASE_URL_DEV,
+      // apiImgUrl: process.env.API_IMG_URL_DEV,
+
+      apiBaseUrl: process.env.API_IMG_URL_PRODUCTION,
+      apiImgUrl: process.env.API_IMG_URL_PRODUCTION,
     },
   },
   routeRules: {
